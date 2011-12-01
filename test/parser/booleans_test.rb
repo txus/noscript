962762be670b057b1565a477e278dc3a69893855
Josep M. Bach <josep.m.bach@gmail.com>
2011-12-01 20:03:30 +0100

    Parse booleans

--- test/parser/booleans_test.rb
+++ test/parser/booleans_test.rb
@@ -1,25 +1,24 @@
 require 'test_helper'
 
 class BooleansTest < MiniTest::Unit::TestCase
-
   def test_true
     parses "true" do |nodes|
       exp = nodes.first
-      assert_equal True.new, exp
+      assert_kind_of TrueNode, exp
     end
   end
 
   def test_false
     parses "false" do |nodes|
       exp = nodes.first
-      assert_equal False.new, exp
+      assert_kind_of FalseNode, exp
     end
   end
 
   def test_nil
     parses "nil" do |nodes|
       exp = nodes.first
-      assert_equal Nil.new, exp
+      assert_kind_of NilNode, exp
     end
   end
 end
