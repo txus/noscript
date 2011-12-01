9084d319cde8543310d5d5e7a37e876f49e8f3e4
Josep M. Bach <josep.m.bach@gmail.com>
2011-12-01 20:02:34 +0100

    Parse boolean expressions as method calls

--- test/parser/boolean_expressions_test.rb
+++ test/parser/boolean_expressions_test.rb
@@ -1,64 +1,16 @@
 require 'test_helper'
 
 class BooleanExpressionsTest < MiniTest::Unit::TestCase
+  %w(== != > >= < <=).each do |operator|
+    define_method(:"test_#{operator}") do
+      parses "foo #{operator} 3" do |nodes|
+        exp = nodes.first
 
-  def test_equality
-    parses "foo == 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of EqualityExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
-    end
-  end
-
-  def test_inequality
-    parses "foo != 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of InequalityExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
-    end
-  end
-
-  def test_gt
-    parses "foo > 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of GtExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
-    end
-  end
-
-  def test_gte
-    parses "foo >= 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of GteExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
-    end
-  end
-
-  def test_lt
-    parses "foo < 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of LtExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
-    end
-  end
-
-  def test_lte
-    parses "foo <= 3" do |nodes|
-      exp = nodes.first
-
-      assert_kind_of LteExpression, exp
-      assert_equal Identifier.new('foo'), exp.lhs
-      assert_equal Integer.new(3), exp.rhs
+        assert_kind_of CallNode, exp
+        assert_equal "foo", exp.receiver
+        assert_equal operator, exp.method
+        assert_equal 3, exp.arguments.first.value
+      end
     end
   end
 end
