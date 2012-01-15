a75aa46751fc105c3237be881b3a626b0c93e0fa
Josep M. Bach <josep.m.bach@gmail.com>
2012-01-15 17:30:08 +0100

    Convenience method from Rexpl

--- lib/noscript/generator.rb
+++ lib/noscript/generator.rb
@@ -1,13 +1,20 @@
 module Noscript
   class Generator < Rubinius::Generator
     def noscript_send(name, args = 0)
-      p "BEFORE #{size}"
       send "noscript:#{name}".to_sym, args
-      p "AFTER #{size}"
     end
 
     def size
       @current_block.instance_eval { @stack }
+    end
+
+    def print
+      return_stack
+      push_const :Rexpl
+      find_const :Output
+      swap_stack
+      send :print_stack, 1, true
+      pop
     end
   end
 end
